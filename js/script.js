104861cf550c4d78fe8278b44e9225218d4b782c
Michael <michael.alesiano@gmail.com>
2021-02-11 14:37:47 +0100

    Milestone 1 - 3

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 var app = new Vue ({
   el : '#app',
   data : {
+    movies : [],
     api_key : 'e80da7051f772c008b17e92730ad435f',
     language : '',
-    query:'',
+    query : '',
+    url_img : 'https://image.tmdb.org/t/p/w342/',
   },
   methods : {
     search() {
       axios
-        .get('https://api.themoviedb.org/3/search/movie?api_key=', {params : {
-          api_key : this.api_key,
-          language : this.language,
-          query : this.query
-        }
-        })
-        .then((result) => {
-          console.log(result.data);
-        });
-    }
+      .get('https://api.themoviedb.org/3/search/movie', {params : {
+        api_key : this.api_key,
+        language : this.language,
+        query : this.query
+      }
+    })
+    .then((result) => {
+      this.movies = result.data.results
+      console.log(this.movies);
+    });
   }
+}
 });
